c25629aaa0f2bda0f0ab92eaad3d8c3c87bd7a6c
Christopher de Beer <christopherdebeer@gmail.com>
2011-09-25 17:22:33 +0200

    progressing with nowjs experiments

--- app.js
+++ app.js
@@ -8,13 +8,31 @@ var nowjs = require("now");
 var app = module.exports = express.createServer();
 var everyone = nowjs.initialize(app);
 
-everyone.now.reserveSq = function (options) {
-  everyone.now.blockSq(options);
+everyone.now.board = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+everyone.now.users = {};
+
+everyone.now.reserveSq = function (userId, options) {
+  everyone.now.clearUserSq(userId)
+  everyone.now.blockSq(userId,options);
+  console.log(userId, "reserved sq: ", options);
 }
 
-everyone.now.makeMove = function (options) {
+everyone.now.makeMove = function (userId, options) {
+  everyone.now.clearUserSq(userId)
   everyone.now.recieveMove(options);
+  console.log(userId, "made a move: ", options);
+  var sq = (parseInt(options.x) * 9) + parseInt(options.y);
+  everyone.now.board[sq] = options.value;
+  console.log("board[" + sq.toString() + "] = " + options.value.toString());
 }
+
+everyone.now.resetBoard = function (userId) {
+  everyone.now.board = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  everyone.now.populateBoard(everyone.now.board);
+  console.log("Board Reset by: ", everyone.now.users[userId]);
+}
+
 
 
 
@@ -44,6 +62,12 @@ app.configure('production', function(){
 app.get('/', function(req, res){
   res.render('index', {});
 });
+
+app.get('/users', function(req, res){
+  res.end("users logged");
+  console.log(everyone.now.users);
+});
+
 
 app.listen(3000);
 console.log("pseudoku server listening on port %d in %s mode", app.address().port, app.settings.env);
--- public/javascripts/pseudoku.js
+++ public/javascripts/pseudoku.js
@@ -4,11 +4,22 @@
 (function($){
 			$(document).ready(function () {
 
+				now.populateBoard = function (board) {
+					$("#board td").each (function(i,el) {
+						if (board[i] != 0) $(el).text(board[i]);
+					});
+				}
 
-				now.blockSq = function (options) {
+				now.clearUserSq = function (userId) {
+					$("#board .user" + userId.toString() )
+						.removeClass("busy")
+						.removeClass("selected");
+				}
+				now.blockSq = function (userId,options) {
 					console.log("Reciveing busy req from server...");
 					$("#board td[data-x='" + options.x.toString() + "'][data-y='" + options.y.toString() + "']")
-						.addClass("busy");
+						.addClass("busy")
+						.addClass("user" + userId.toString());	
 				}
 
 				now.recieveMove = function (options) {
@@ -85,51 +96,69 @@
 					return true;
 				}
 
-				// setup board
-				for (var x = 0; x < 9; x++) {
-					$("#board").append("<tr id='row" + x.toString() +"'/>");
-					for (var y = 0; y < 9; y++) {
-						$("#row" + x.toString()).append("<td data-x='" + x.toString() + "' data-y='" + y.toString() + "' ></td>")
+
+				$("#board").append("<tr><td>Connecting...</td><tr>");
+				now.ready( function() {
+					
+
+
+
+					now.users[now.core.clientId] = prompt("What username do you want to use?", "guest" + now.core.clientId.toString());
+					// setup board
+
+					$("#board").html("");
+					for (var x = 0; x < 9; x++) {
+						$("#board").append("<tr id='row" + x.toString() +"'/>");
+						for (var y = 0; y < 9; y++) {
+							$("#row" + x.toString()).append("<td data-x='" + x.toString() + "' data-y='" + y.toString() + "' ></td>")
+						}
 					}
-				}
 
-				// square click events
-				$("#board td").click(function(e) {
-					if (!$(this).hasClass("done") && !$(this).hasClass("busy")) {
-						$("#board td").removeClass("selected");
-						now.reserveSq({
-							x: $(this).attr("data-x"),
-							y: $(this).attr("data-y")
-						});
-						$(this).addClass("selected")
-							.attr("style","")
-						$("#numPad").show();
-					}
-				});
+					now.populateBoard(now.board);
+
+					// square click events
+					$("#board td").click(function(e) {
+						if (!$(this).hasClass("done") && !$(this).hasClass("busy")) {
+							$("#board td").removeClass("selected");
+							now.reserveSq(now.core.clientId,{
+								x: $(this).attr("data-x"),
+								y: $(this).attr("data-y")
+							});
+							$(this).addClass("selected")
+								.attr("style","")
+							$("#numPad").show();
+						}
+					});
 
 
-				// numPad click events
-				$("#numPad a").click(function(e) {
-					if ($(this).text() == "Cancel") {
+					// numPad click events
+					$("#numPad a").click(function(e) {
+						if ($(this).text() == "Cancel") {
+							e.preventDefault();
+							$("#numPad").hide();
+							$("#board td").removeClass("selected");
+						} else {
+							e.preventDefault();
+							var thisX = $("td.selected").attr("data-x");
+							var thisY = $("td.selected").attr("data-y");
+							var newValue = $(this).text();
+
+							now.makeMove(now.core.clientId,{
+								x: thisX,
+								y: thisY,
+								value: newValue
+							});
+						}
+					});
+
+					// reset button
+
+					$(".reset").click(function(e){
 						e.preventDefault();
-						$("#numPad").hide();
-						$("#board td").removeClass("selected");
-					} else {
-						e.preventDefault();
-						var thisX = $("td.selected").attr("data-x");
-						var thisY = $("td.selected").attr("data-y");
-						var newValue = $(this).text();
-						// updateSq({
-						// 	x: thisX,
-						// 	y: thisY,
-						// 	value: newValue 
-						// });
-						now.makeMove({
-							x: thisX,
-							y: thisY,
-							value: newValue
-						});
-					}
+						now.resetBoard(now.core.clientId);
+						return false;
+					});
+
 				});
 
 			});
