f4b121f9fcac2b88435b47f885e5b22b356a8784
Christopher de Beer <christopherdebeer@gmail.com>
2011-09-25 14:03:21 +0200

    tidied table markup/spacing

--- app.js
+++ app.js
@@ -29,9 +29,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
+  res.render('index', {});
 });
 
 app.listen(3000);
